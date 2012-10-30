e065ef38467d5476c15243a7a543180265a3290c
Ubasic <lmjubuntu@gmail.com>
2012-10-30 21:01:02 +0800

    update output log

--- main.py
+++ main.py
@@ -42,11 +42,13 @@ def get_relation(uid):
     fans = []
     for fan in weibo.get_hisfans(uid):
         fans.append(fan['uid'])
+        print('User[%s] fan[%s]'%(uid, fan['uid']))
         db.add_user(fan['uid'], fan['nickname'], fan['sex'], fan['address'], fan['info'], fan['face'])
         db.add_relation(uid, fan['uid'])
     follow = []
     for fo in weibo.get_hisfollow(uid):
         follow.append(fo['uid'])
+        print('User[%s] follow[%s]'%(uid, fo['uid']))
         db.add_user(fo['uid'], fo['nickname'], fo['sex'], fo['address'], fo['info'], fo['face'])
         db.add_relation(fo['uid'], uid)
     for friend in get_friends(fans, follow):
